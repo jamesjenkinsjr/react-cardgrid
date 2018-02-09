810bf12e2fcf9d8b071badcb62aac88031adb4cc
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-08 19:02:23 -0500

    Created basic card object data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super(); //This makes sure that the app becomes a component
+    this.state = {
+      cards: [
+        {
+          src: 'https://via.placeholder.com/150x350',
+          caption: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum',
+          isLiked: true
+        }, 
+        {
+          src: 'https://via.placeholder.com/150x350',
+          caption: 'Wow wow wow wow wow wow wow wow',
+          isLiked: false
+        }
+      ]
+    }
+  }
   render() {
+    console.log(this.state);
     return (
-        <h1>Hello</h1>
+        <div className="container">
+          <h1>CardGrid</h1>
+
+        </div>
     );
   }
 }
