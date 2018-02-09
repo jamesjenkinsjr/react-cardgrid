d75160bc95b432637a45e4e8d4dfb95efcf69d14
James Jenkins <jamesjenkinsjr@icloud.com>
2018-02-09 18:18:09 -0500

    Ternary classes and styles for isLiked

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 
 class Card extends Component {
     render() {
-        let style;
-        if(this.props.isLiked) {
-            style = {color: 'skyblue'};
-        } else {
-            style = {color: 'grey'};
+        //Look up and think about the ternary operator.  Sometimes known as 'elvis' operator
+        const style = {
+            color: this.props.isLiked ? 'skyblue' : 'grey',
         }
+        const classes = this.props.isLiked ? 'fa fa-heart' : 'fa fa-heart-o';
         return (
             <div className="col-md-3 my-1">
                 <img src={this.props.src} alt={this.props.caption} />
                 <p>{this.props.caption}</p>
-                <i alt="heart" className="fa fa-heart" aria-hidden="true" style={style} />
+                <i alt="heart" className={classes} aria-hidden="true" style={style} />
             </div>
         );
     }
